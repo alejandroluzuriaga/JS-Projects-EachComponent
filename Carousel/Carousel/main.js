35ca496fb2d3ab049ab89ebba7c29c0c7101a2c2
alejandroluzuriaga <luzuriagalejandro@gmail.com>
2022-12-16 16:18:50 +0100

    Building Carousel

--- Carousel/Carousel/main.js
+++ Carousel/Carousel/main.js
@@ -18,11 +18,12 @@ const scrollableSet = document.querySelector('.scrollable-set');
 const imagePrevElement = document.querySelector('.image-preview');
 const getscrollableElementTemplate = (image, index) =>{
   return `
-  <li>
+  <li role="button" class = "clickable">
     <img id='image-${index}'src="${image.src}" alt="${image.alt}" />
   </li>`
 }
-
+let actualImageIndex = 0;
+let imageInterval;
 
 const setupScrollableSet = () =>{
   IMAGES.forEach((image, index) =>{
@@ -31,4 +32,42 @@ const setupScrollableSet = () =>{
   })
 }
 
+const setupCarouselInterval = () =>{
+   imageInterval = setInterval(() =>{
+    (actualImageIndex === IMAGES.length-1) ?  actualImageIndex = 0 : actualImageIndex += 1;
+    setupImagePreview(IMAGES[actualImageIndex].src);
+  }, 4000)
+}
+
+const resetCarouselPreview = () =>{
+  clearInterval(imageInterval);
+  setupCarouselInterval();
+}
+
+const setupImagePreview = (src) =>{
+  imagePrevElement.style.backgroundImage = `url(${src})`;
+  const selectedImage = document.querySelector(`img[src="${src}"]`);
+  selectedImage.scrollIntoView({behavior: 'smooth'});
+  
+  resetCarouselPreview();
+}
+
+const handleChangePreview = (event) =>{
+  const image = event.target;
+  const imageIndex = Number(image.id.split('-')[1]);
+  actualImageIndex = imageIndex;
+
+  setupImagePreview(image.getAttribute('src'));
+}
+
+const addScrollableListeners = () =>{
+  const scrollables = document.querySelectorAll('li.clickable');
+  scrollables.forEach((scrollable)=> scrollable.addEventListener('click', handleChangePreview));
+
+}
+
+
 setupScrollableSet();
+setupImagePreview(IMAGES[0].src);
+addScrollableListeners();
+setupCarouselInterval();
